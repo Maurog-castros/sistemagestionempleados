24f5ea6c43ce8d4cd30ef10f8b661125339eba5e
Mauricio CASTRO <mauricio.castro@geodis.com>
2024-10-14 20:26:35 -0300

    Update Mauro

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from empresa.gestion.gestion_empleado import GestionEmpleado
 from empresa.DAO.conexion_db import ConexionDB
 from empresa.DTO.DTOpersona import Persona
 from empresa.DTO.usuario import Usuario
+from empresa.DAO.DAOpersona import DAOPersona
 
 def main():
 
@@ -10,15 +11,10 @@ def main():
 #CLAE - CREAR, LEER, ACTUALIZAR, ELIMINAR   
 
 # Crear una persona en la base de datos
-    persona_uno = Persona()
-    persona_uno.crear_persona_db("Juan", "Perez", "juan.perez@ejemplo.com", "123456789")
+    CrearPersona = DAOPersona()
+    CrearPersona.crear_persona_db("Juan", "Perez", "juan.perez@ejemplo.com", "123456789")
 
 #aca se demuestra la herencia de la clase usuario
-    usuario = Usuario(
-        id_usuario=persona_uno._Persona__id_persona,
-        nombre_usuario=persona_uno._Persona__correo.split('@')[0],
-        contrasena="contraseña_temporal",
-        persona=persona_uno
-)
+    
 if __name__ == '__main__':
     main()
